ea9c8f68df66ae72fc13398e736c124b7be94619
Sergey Dubovyk <dubovyk@ucu.edu.ua>
2016-08-31 23:58:17 +0300

    New file structure for setup.py

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt-get install ffmpeg
+sudo apt-get install sox
+sudo install libavcodec-extra-*
+sudo python setup.py install
--- setup.py
+++ setup.py
@@ -6,16 +6,20 @@
 
 import re
 from setuptools import setup
+from pip.req import parse_requirements
 
+install_reqs = parse_requirements('requirements.txt', session=False)
+
+reqs = [str(ir.req) for ir in install_reqs]
 
 version = re.search(
     '^__version__\s*=\s*"(.*)"',
-    open('scvid.py').read(),
+    open('scvid/scvid.py').read(),
     re.M
 ).group(1)
 
 
-with open("README.md", "rb") as f:
+with open("scvid/README.md", "rb") as f:
     long_descr = f.read().decode("utf-8")
 
 
@@ -23,12 +27,13 @@ setup(
     name="scvid-lin",
     packages=["scvid"],
     entry_points={
-        "console_scripts": ['scvid = scvid:main']
+        "console_scripts": ['scvid = scvid.scvid:main']
     },
     version=version,
+    install_requires=reqs,
     description="Python command line application for creating video from live sceenshots.",
     long_description=long_descr,
     author="Serhii Dubovyk",
     author_email="sergeydubovick@gmail.com",
-    url="",
+    url="https://github.com/dubovyk/scvid/",
 )
